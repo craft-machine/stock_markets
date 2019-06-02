8c97f5e21a00d83d3a1c9abcf48a456fbdab5f52
Kirill Tatchihin <kirabsuir@gmail.com>
2019-06-02 22:20:58 +0300

    Remove useless code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,4 @@ require 'mocha/minitest'
 
 include WebMock::API
 
-file_data = File.read('test/helpers/example_data.csv')
 WebMock.disable_net_connect!(allow: Regexp.new("https://www.iso20022.org"))
-stub_request(:any, Regexp.new("https://www.iso20022.org")).with(body: file_data)
